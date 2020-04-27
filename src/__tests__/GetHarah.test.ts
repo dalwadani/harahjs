f840a668a2d48669b772d5a66181d34fa24704e7
Dhaifallah Alwadani <dalwadani@gmail.com>
2020-04-28 02:49:35 +0300

    version 1

--- src/__tests__/GetHarah.test.ts
+++ src/__tests__/GetHarah.test.ts
@@ -1,5 +1,11 @@
 import { GetHarah } from '../index';
 
-test('Get Harah', () => {
-  expect(GetHarah({ latitude: 24.831143, longitude: 46.687396 })).toBe('Al Narjis');
+test('Get a Harah', () => {
+  expect(GetHarah({ latitude: 24.831143, longitude: 46.687396 })).toEqual({
+    district_id: 10100003150,
+    city_id: 3,
+    region_id: 1,
+    name_ar: 'حي النرجس',
+    name_en: 'Al Narjis Dist.',
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,49 @@
-export function GetHarah({ latitude, longitude }: { latitude: number; longitude: number }) {
-  throw 'NOT IMPLEMENTED';
+import districts from './districts.json';
+
+interface area {
+  district_id: number;
+  city_id: number;
+  region_id: number;
+  name_ar: string;
+  name_en: string;
+}
+
+interface point {
+  latitude: number;
+  longitude: number;
+}
+
+export function GetHarah({ latitude, longitude }: point): area {
+  const area: any = districts.find((x: any) => {
+    return is_inside([latitude, longitude], x.boundaries[0]);
+  });
+  const { district_id, city_id, region_id, name_ar, name_en } = area;
+  return {
+    district_id,
+    city_id,
+    region_id,
+    name_ar,
+    name_en,
+  };
+}
+
+function is_inside(point: [number, number], vs: [number, number][]) {
+  // ray-casting algorithm based on
+  // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+
+  var x = point[0],
+    y = point[1];
+
+  var inside = false;
+  for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+    var xi = vs[i][0],
+      yi = vs[i][1];
+    var xj = vs[j][0],
+      yj = vs[j][1];
+
+    var intersect = yi > y != yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+    if (intersect) inside = !inside;
+  }
+
+  return inside;
 }
